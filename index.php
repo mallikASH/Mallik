81852ab54e5d651ddb350ea27865529758b06a73
429273-2 <429273-2>
2019-11-20 13:27:31 +0300

    Lab 2

--- index.php
+++ index.php
@@ -1,21 +1,72 @@
-<?php
-class A{
+class RudikovakotovaException extends RuntimeException {
+}
+class A {
+	protected a;
+	protected b;
+	protected x;
+	function __construct($a, $b){
+		$this->a=$a;
+		$this->b=$b;
+	}
+	function ur($a, $b){
+		if ($a != 0) {
+			$x = -1*$b/$a;
+			$this->x = $x;
+			return $x;
+		}
+		throw new RudikovakotovaException ("нет решения");	
+	}
 }
 class B extends A{
-	public a;
-	function __construct($a){
-		$this->a = $a;
-	}
-}
-class C extends B{
-	public b;
-	public c;
-	function __construct($a, $b,$c){
-		parent::__construct($a);
-		$this->b=$b;
+	protected c;
+	protected x2;
+	function __construct($a, $b, $c){
+		parent::__construct($a, $b);
 		$this->c=$c;
 	}
+	protected function nv(($a, $b, $c) {
+		$nv = $b*$b - 4*$a*$c;
+		return $nv;
+	}
+	function ur2(($a, $b, $c) {
+		if ($a==0) {
+		$this->ur($a,$b);
+		}
+		$nv = $this->nv();
+		if ($nv >0){
+			$x = (-1*$b + sqrt($nv))/(2*$a);
+			$x2 = (-1*$b - sqrt($nv))/(2*$a);
+			$this->x = $x;
+			$this->x2 = $x2;
+			return array($x, $x2);
+		} else ($nv = 0) {
+			$x = (-1*$b)/(2*$a);
+			$this->x = $x;
+			return array($x);
+		}
+		throw new RudikovakotovaException ("нет решения");
+	}
 }
-$b= new A();
-$a= new C(new A(), new B(new B($b)), $b);
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
